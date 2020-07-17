52c0bc9383fe4f22fbedf1913b6f37799cae81ca
mostlypanda <sarthakmittal1461@gmail.com>
2020-07-17 17:14:56 +0530

    first route

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,10 @@
-const express=require()
+const express=require('express');
+const router=express.Router();
+
+
+router.get('/',(req,res)=>{
+    res.send('welcome');
+});
+
+
+module.exports=router;
--- server.js
+++ server.js
@@ -2,12 +2,17 @@ const express=require('express');
 const app =express();
 const expresslayouts=require('express-ejs-layouts');
 
+
+const indexrouter=require('./routes/index');
+
+
 app.set('view engine','ejs');
 app.set('views',__dirname+'/views');
 app.set('layout','layouts/layout');
 app.use(expresslayouts);
 app.use(express.static('public'));
 
+app.use('/',indexrouter);
 
 
 app.listen(process.env.PORT||3000,()=>{
