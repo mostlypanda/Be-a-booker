2781c350ff9dc96f23a5ad2d752ead201a23fa7c
mostlypanda <sarthakmittal1461@gmail.com>
2020-07-19 23:29:53 +0530

    blog

---
+++ routes/blogs.js
@@ -0,0 +1,19 @@
+const express=require('express');
+const router=express.Router();
+const Blog=require('../models/blog');
+
+// All blogs
+router.get('/',async (req,res)=>{
+   
+});
+
+// new blog
+router.get('/new', (req,res)=>{
+  });
+
+// create a blog
+router.post('/',async (req,res)=>{
+   
+});
+
+module.exports=router;
