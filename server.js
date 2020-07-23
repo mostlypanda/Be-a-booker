ae2aae6354861d2d79c3ba1e5afe45280e30a53b
mostlypanda <sarthakmittal1461@gmail.com>
2020-07-23 12:57:52 +0530

    app is live

---
+++ server.js
@@ -0,0 +1,33 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').load()
+}
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+const bodyParser = require('body-parser')
+const methodOverride = require('method-override')
+
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const blogRouter = require('./routes/blogs')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(methodOverride('_method'))
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://test:test@cluster0-bi1rv.mongodb.net/bloggerapp?retryWrites=true&w=majority', { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
+
+app.use('/', indexRouter)
+app.use('/authors', authorRouter)
+app.use('/blogs', blogRouter)
+
+app.listen(process.env.PORT || 3000)
